3a63cd17779d7b12eba379b8191c4eb455331b67
Tigran <41443306+septoon@users.noreply.github.com>
2023-04-17 18:38:11 +0300

    added content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import { images } from './assets/data-img';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {
+      images.map(item => {
+        return (
+          <img className="image" src={item.url} alt={item.alt} />
+        )
+      })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/assets/data-img.js
@@ -0,0 +1,117 @@
+export const images = [
+  {
+    "id": 1,
+    "alt": "",
+    "url": "https://i.ibb.co/yFMSDFQ/01.jpg"
+  },
+  {
+    "id": 2,
+    "alt": "",
+    "url": "https://i.ibb.co/SRyYkTT/02.jpg"
+  },
+  {
+    "id": 3,
+    "alt": "",
+    "url": "https://i.ibb.co/Kqrfd6H/03.jpg"
+  },
+  {
+    "id": 4,
+    "alt": "",
+    "url": "https://i.ibb.co/4Kj3V8j/04.jpg"
+  },
+  {
+    "id": 5,
+    "alt": "",
+    "url": "https://i.ibb.co/j3Td61m/05.jpg"
+  },
+  {
+    "id": 6,
+    "alt": "",
+    "url": "https://i.ibb.co/hXV5HfS/06.jpg"
+  },
+  {
+    "id": 7,
+    "alt": "",
+    "url": "https://i.ibb.co/Rbcc7HB/07.jpg"
+  },
+  {
+    "id": 8,
+    "alt": "",
+    "url": "https://i.ibb.co/J5WxcDh/08.jpg"
+  },
+  {
+    "id": 9,
+    "alt": "",
+    "url": "https://i.ibb.co/NVhTjVV/09.jpg"
+  },
+  {
+    "id": 10,
+    "alt": "",
+    "url": "https://i.ibb.co/cc8pRzG/10.jpg"
+  },
+  {
+    "id": 11,
+    "alt": "",
+    "url": "https://i.ibb.co/YdS0jYV/11.jpg"
+  },
+  {
+    "id": 12,
+    "alt": "",
+    "url": "https://i.ibb.co/52NCKz3/12.jpg"
+  },
+  {
+    "id": 13,
+    "alt": "",
+    "url": "https://i.ibb.co/Y7Nj2fr/13.jpg"
+  },
+  {
+    "id": 14,
+    "alt": "",
+    "url": "https://i.ibb.co/H24vqXy/14.jpg"
+  },
+  {
+    "id": 15,
+    "alt": "",
+    "url": "https://i.ibb.co/SKGfnz8/15.jpg"
+  },
+  {
+    "id": 16,
+    "alt": "",
+    "url": "https://i.ibb.co/PmC6NjD/16.jpg"
+  },
+  {
+    "id": 17,
+    "alt": "",
+    "url": "https://i.ibb.co/VjQDtJq/17.jpg"
+  },
+  {
+    "id": 18,
+    "alt": "",
+    "url": "https://i.ibb.co/7kdbVgk/18.jpg"
+  },
+  {
+    "id": 19,
+    "alt": "",
+    "url": "https://i.ibb.co/jg7xRNr/19.jpg"
+  },
+  {
+    "id": 20,
+    "alt": "",
+    "url": "https://i.ibb.co/xXTnpHv/20.jpg"
+  },
+  {
+    "id": 21,
+    "alt": "",
+    "url": "https://i.ibb.co/L9yzgVb/21.jpg"
+  },
+  {
+    "id": 22,
+    "alt": "",
+    "url": "https://i.ibb.co/Ltp9XNB/23.jpg"
+  },
+  {
+    "id": 23,
+    "alt": "",
+    "url": "https://i.ibb.co/3mYGFcw/24.jpg"
+  },
+]
